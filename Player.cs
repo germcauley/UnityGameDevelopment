e58cad1905edd1526fa9c9ebd0853b3cd548f0c7
Gerald McAuley <geraldmcauley@gmail.com>
2018-05-29 23:27:00 +0100

    Player Move script unfinished

---
+++ Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 8f, maxVelocity= 4f;
+    
+	private Rigidbody2D myBody;
+	private Animator animator;
+
+	void Awake()
+	{
+		myBody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void PlayerMoveKeyboard(){
+		float forceX = 0f;
+		float vol = Mathf.Abs(myBody.velocity.x);
+	}
+}
